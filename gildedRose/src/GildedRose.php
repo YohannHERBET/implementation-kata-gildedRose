a0242910b58563156e0af94e8e2258520902a4fd
YohannHERBET <yohannherbet.pro@gmail.com>
2024-08-07 11:46:21 +0200

    comment and integration test

--- gildedRose/src/GildedRose.php
+++ gildedRose/src/GildedRose.php
@@ -17,23 +17,37 @@ final class GildedRose
     public function updateQuality(): void
     {
         foreach ($this->items as $item) {
+            // si le nom de l'item n'est pas "Aged Brie" et n'est pas "Backstage passes to a TAFKAL80ETC concert"
             if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+                // et que la qualité de l'item est supérieure à 0
                 if ($item->quality > 0) {
+                    // et que le nom de l'item n'est pas "Sulfuras, Hand of Ragnaros"
                     if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+                        // alors la qualité de l'item est décrémentée
                         $item->quality = $item->quality - 1;
                     }
                 }
             } else {
+                // sinon
+                // si la qualité de l'item est inférieure à 50
                 if ($item->quality < 50) {
+                    // alors la qualité de l'item est incrémentée
                     $item->quality = $item->quality + 1;
+                    // si le nom de l'item est "Backstage passes to a TAFKAL80ETC concert"
                     if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
+                        // et que les jours restants pour vendre l'item sont inférieurs à 11
                         if ($item->sellIn < 11) {
+                            // et que la qualité de l'item est inférieure à 50
                             if ($item->quality < 50) {
+                                // alors la qualité de l'item est incrémentée
                                 $item->quality = $item->quality + 1;
                             }
                         }
+                        // si les jours restants pour vendre l'item sont inférieurs à 6
                         if ($item->sellIn < 6) {
+                            // et que la qualité de l'item est inférieure à 50
                             if ($item->quality < 50) {
+                                // alors la qualité de l'item est incrémentée
                                 $item->quality = $item->quality + 1;
                             }
                         }
@@ -41,23 +55,36 @@ final class GildedRose
                 }
             }
 
+            // si le nom de l'item n'est pas "Sulfuras, Hand of Ragnaros"
             if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+                // alors les jours restants pour vendre l'item sont décrémentés
                 $item->sellIn = $item->sellIn - 1;
             }
 
+            // si les jours restants pour vendre l'item sont inférieurs à 0
             if ($item->sellIn < 0) {
+                // et que le nom de l'item n'est pas "Aged Brie"
                 if ($item->name != 'Aged Brie') {
+                    // et que le nom de l'item n'est pas "Backstage passes to a TAFKAL80ETC concert"
                     if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+                        // et que la qualité de l'item est supérieure à 0
                         if ($item->quality > 0) {
+                            // et que le nom de l'item n'est pas "Sulfuras, Hand of Ragnaros"
                             if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+                                // alors la qualité de l'item est décrémentée
                                 $item->quality = $item->quality - 1;
                             }
                         }
                     } else {
+                        // sinon
+                        // la qualité de l'item est décrémentée
                         $item->quality = $item->quality - $item->quality;
                     }
                 } else {
+                    // sinon
+                    // si la qualité de l'item est inférieure à 50
                     if ($item->quality < 50) {
+                        // alors la qualité de l'item est incrémentée
                         $item->quality = $item->quality + 1;
                     }
                 }
--- gildedRose/tests/GildedRoseTest.php
+++ gildedRose/tests/GildedRoseTest.php
@@ -10,11 +10,85 @@ use PHPUnit\Framework\TestCase;
 
 class GildedRoseTest extends TestCase
 {
-    public function testFoo(): void
+    private array $items;
+    private GildedRose $gildedRose;
+    
+    protected function setUp(): void
     {
-        $items = [new Item('foo', 0, 0)];
-        $gildedRose = new GildedRose($items);
-        $gildedRose->updateQuality();
-        $this->assertSame('fixme', $items[0]->name);
+        $this->items = [
+            new Item('+5 Dexterity Vest', 10, 20),
+            new Item('Aged Brie', 2, 0),
+            new Item('Elixir of the Mongoose', 5, 7),
+            new Item('Sulfuras, Hand of Ragnaros', 0, 80),
+            new Item('Sulfuras, Hand of Ragnaros', -1, 80),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49),
+            new Item('Conjured Mana Cake', 3, 6),
+        ];
+        $this->gildedRose = new GildedRose($this->items);
+    }
+    public function testUpdateQualityForTwoDays(): void
+    {
+
+        // Day 1
+        $this->gildedRose->updateQuality();
+
+        $this->assertEquals(9, $this->items[0]->sellIn);
+        $this->assertEquals(19, $this->items[0]->quality);
+
+        $this->assertEquals(1, $this->items[1]->sellIn);
+        $this->assertEquals(1, $this->items[1]->quality);
+
+        $this->assertEquals(4, $this->items[2]->sellIn);
+        $this->assertEquals(6, $this->items[2]->quality);
+
+        $this->assertEquals(0, $this->items[3]->sellIn);
+        $this->assertEquals(80, $this->items[3]->quality);
+
+        $this->assertEquals(-1, $this->items[4]->sellIn);
+        $this->assertEquals(80, $this->items[4]->quality);
+
+        $this->assertEquals(14, $this->items[5]->sellIn);
+        $this->assertEquals(21, $this->items[5]->quality);
+
+        $this->assertEquals(9, $this->items[6]->sellIn);
+        $this->assertEquals(50, $this->items[6]->quality);
+
+        $this->assertEquals(4, $this->items[7]->sellIn);
+        $this->assertEquals(50, $this->items[7]->quality);
+
+        $this->assertEquals(2, $this->items[8]->sellIn);
+        $this->assertEquals(5, $this->items[8]->quality);
+
+        // Day 2
+        $this->gildedRose->updateQuality();
+
+        $this->assertEquals(8, $this->items[0]->sellIn);
+        $this->assertEquals(18, $this->items[0]->quality);
+
+        $this->assertEquals(0, $this->items[1]->sellIn);
+        $this->assertEquals(2, $this->items[1]->quality);
+
+        $this->assertEquals(3, $this->items[2]->sellIn);
+        $this->assertEquals(5, $this->items[2]->quality);
+
+        $this->assertEquals(0, $this->items[3]->sellIn);
+        $this->assertEquals(80, $this->items[3]->quality);
+
+        $this->assertEquals(-1, $this->items[4]->sellIn);
+        $this->assertEquals(80, $this->items[4]->quality);
+
+        $this->assertEquals(13, $this->items[5]->sellIn);
+        $this->assertEquals(22, $this->items[5]->quality);
+
+        $this->assertEquals(8, $this->items[6]->sellIn);
+        $this->assertEquals(50, $this->items[6]->quality);
+
+        $this->assertEquals(3, $this->items[7]->sellIn);
+        $this->assertEquals(50, $this->items[7]->quality);
+
+        $this->assertEquals(1, $this->items[8]->sellIn);
+        $this->assertEquals(4, $this->items[8]->quality);
     }
 }
